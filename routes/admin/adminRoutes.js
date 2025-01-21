6d1c1ae2f8c667f714e18b9e481c93e7ccfc315c
marcus-nguyen <imkay2712@gmail.com>
2025-01-21 15:49:51 +0700

    handler document manage

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -4,6 +4,7 @@ const categoryRoutes = require("../../routes/admin/category/categoryRoutes");
 const serviceRoutes = require("../../routes/admin/service/serviceRoutes");
 const profieRoutes = require("../../routes/admin/profile/profileRoutes");
 const proceduresRoutes = require("../../routes/procedures/proceduresRoutes");
+const documentsRoutes = require("../../routes/documents/documentsRoutes");
 const customersRoutes = require("../../routes/admin/customers/customersRoutes");
 const accountRoutes = require("../../routes/admin/account/accountRoutes");
 const customerRoutes = require("../../routes/admin/customer/customerRoutes");
@@ -18,6 +19,8 @@ router.use("/profile", profieRoutes);
 router.use("/ticket", ticketRoutes);
 
 router.use("/procedure", proceduresRoutes);
+
+router.use("/documents", documentsRoutes);
 
 router.use("/customers", customersRoutes);
 
