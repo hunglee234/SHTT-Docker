13493a9b1b1a82c5236802b41b01b86821be39c6
marcus-nguyen <imkay2712@gmail.com>
2025-01-25 10:06:14 +0700

    document for client

--- routes/user/routesUser.js
+++ routes/user/routesUser.js
@@ -7,6 +7,7 @@ const serviceRoutes = require("../../routes/user/service/serviceRoutes");
 const upload = require("../../utils/multer");
 const regisService = require("../../routes/sevice/serviceRouter");
 const proceduresRoutes = require("../../routes/procedures/proceduresRoutes");
+const documentsUserRoutes = require("../../routes/documents/documentsUserRoutes");
 const notiRoutes = require("../../routes/noti/notiRoutes");
 
 router.use("/category", categoryRoutes);
@@ -16,6 +17,8 @@ router.use("/service", serviceRoutes);
 router.use("/ticket", ticketRoutes);
 
 router.use("/procedure", proceduresRoutes);
+
+router.use("/documents", documentsUserRoutes);
 
 router.use("/noti", notiRoutes);
 // Lấy thông tin tài khoản
